cb246189b5c7243f70ac287f3ff18b2c6387e262
Henning Möllers <henning.moellers@wps.de>
2024-03-01 10:58:25 +0100

    Create RiskApi application.

---
+++ leasingninja-riskApi/src/main/java/io/leasingninja/riskapi/RiskApi.java
@@ -0,0 +1,14 @@
+package io.leasingninja.riskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RiskApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskApi.class, args);
+	}
+
+}
